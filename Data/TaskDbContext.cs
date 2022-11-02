b4bc4f173c6ae77bd8b0f9a24f31ff9799aeef17
amirzulic <nokia5130>
2022-11-02 19:41:05 +0100

    Database connection and Task model

---
+++ Data/TaskDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = FirstApi.Models.Task;
+
+namespace FirstApi.Data
+{
+    public class TaskDbContext : DbContext
+    { 
+        public TaskDbContext(DbContextOptions<TaskDbContext> options)
+            : base(options) 
+        {
+
+        }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstApi.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public TaskType TaskType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+
+    public enum TaskType
+    {
+        Work, Family, Friends
+    }
+}
